05492726bf9c45f2b6ddb2baa4240dd66231daf6
Ingrid <ingrita.rpm@gmail.com>
2019-10-14 23:58:07 -0600

    Registro - Prueba 4 falla
    Prueba de registro sin apellido falla

--- app-banco/general/Pruebas/Registro/PruebaRegistro.php
+++ app-banco/general/Pruebas/Registro/PruebaRegistro.php
@@ -14,8 +14,10 @@ class PruebaRegistro extends TestCase
   public function testRegistroIncorrecto(){
     //Prueba 2: Sin DPI
     $this->assertSame(false,crear_usuario("Andrea Sofia", "Molina Rodriguez", null, 100.00, "andrea@correo.com", "123"));
-    //Prueba 2: Sin nombre
+    //Prueba 3: Sin nombre
     $this->assertSame(false,crear_usuario(null, "Molina Rodriguez", 3025162585025, 100.00, "andrea@correo.com", "123"));
+    //Prueba 4: Sin apellidos
+    $this->assertSame(false,crear_usuario("Andrea Sofia", null, 3025162585025, 100.00, "andrea@correo.com", "123"));
   }
 }
 
