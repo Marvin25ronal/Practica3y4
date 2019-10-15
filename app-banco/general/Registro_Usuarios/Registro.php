e6ab2ce55e4ac748b71ac8b21cd3cff39c8e4fc0
Ingrid <ingrita.rpm@gmail.com>
2019-10-14 23:52:18 -0600

    Registro - Prueba 2 pasa
    Después de escribir el código para limitar que el dpi no sea nulo, la prueba pasa correctamente

--- app-banco/general/Registro_Usuarios/Registro.php
+++ app-banco/general/Registro_Usuarios/Registro.php
@@ -9,6 +9,9 @@
 		return false;
 	}
 	function create_user($nombres, $apellidos, $dpi, $saldo, $correo, $contrasena){
+		if($dpi==null){
+ 			return "Error al registrarse, el campo 'DPI' debe llenarse obligatoriamente.";
+ 		}
 		$res = agregar_usuario($nombres, $apellidos, $dpi, $saldo, $correo, $contrasena);
 		if($res == 1){
 			return "";
