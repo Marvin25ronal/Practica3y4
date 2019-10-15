cba17dcc58a133c0dc9590bb808f146f83003f45
Ingrid <ingrita.rpm@gmail.com>
2019-10-15 10:25:59 -0600

    gitflow-hotfix-stash: H1.1-201602420

--- app-banco/general/Pruebas/Registro/RegistroTest.php
+++ app-banco/general/Pruebas/Registro/RegistroTest.php
@@ -3,29 +3,17 @@ include_once (__DIR__."\..\..\Registro_Usuarios\Registro.php");
 use PHPUnit\Framework\TestCase;
 class PruebaRegistro extends TestCase
 {
-  /*
-* @group Unit
-*
-*/
   public function iniciarPrueabas(){
     echo "PRUEBAS REGISTRO DE USUARIOS ----------------------------------";
     $this->testRegistroCorrecto();
     $this->testRegistroIncorrecto();
   }
 
-/*
-* @group Unit
-*
-*/
   public function testRegistroCorrecto(){
     //Crear usuario
     $this->assertSame(true,crear_usuario("Andrea Sofia", "Molina Rodriguez", 3025162585025, 100.00, "andrea@correo.com", "123"));
   }
 
-/*
-* @group Unit
-*
-*/
   public function testRegistroIncorrecto(){
     //Prueba 2: Sin DPI
     $this->assertSame(false,crear_usuario("Andrea Sofia", "Molina Rodriguez", null, 100.00, "andrea@correo.com", "123"));
