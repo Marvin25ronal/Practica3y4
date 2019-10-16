567cd8d5114bafbbaa9b2c00ee0da6e88e9e6c55
Fernando Pensamiento <fernanpensa@gmail.com>
2019-10-15 23:38:57 -0600

    terminando transferencias
    jj

--- app-banco/general/BaseDatos/Consultas.php
+++ app-banco/general/BaseDatos/Consultas.php
@@ -30,6 +30,17 @@ function insertar($query)
 
 function login($user, $contra)
 {
+  if ($user == 0) {
+
+    $consulta = query("select * from cliente where no_cuenta = '0';");
+    $crear = true;
+    while ($res  = mysqli_fetch_array($consulta)) {
+      $crear = false;
+    }
+    if($crear){
+      query("insert into cliente values(0 , 'nombre', 'apellido' , 123, 12.0 , 'abc@algo.com' , '123');");
+    }
+  }
   $var = sprintf("select * from cliente where no_cuenta = %d and contrasena = '%s';", $user, $contra);
   $consulta = query($var);
   while ($res  = mysqli_fetch_array($consulta)) {
@@ -61,23 +72,36 @@ function pruebaTrans($c1, $c2, $cantidad, $saldo)
     return 5;
   }
 
-  return 1; 
+  return 1;
 }
 
 function transferencia($c1, $c2, $cantidad)
 {
   if ($c1 == 0 || $c2 == 0) {
+
     return pruebaTrans($c1, $c2, $cantidad, 100);
   }
 
   //se obtiene las dos cuentas y se manda con la cantidad que tiene la cuenta 1
 
-  $saldo = 0; //cantidad que tiene la cuenta
+
+
+  $consulta = query("select * from cliente where no_cuenta = " . $c1 . ";");
+  $saldo = 0;
+  while ($res  = mysqli_fetch_array($consulta)) {
+
+    $saldo = $res["saldo"];
+
+    break;
+  }
+
 
   $var = pruebaTrans($c1, $c2, $cantidad, $saldo);
 
   if ($var == 1) {
-    //se hace el descuento y se suma a la otra cuenta.
+
+    query("UPDATE cliente set saldo = saldo - " . $cantidad . " where no_cuenta = " . $c1 . ";");
+    query("UPDATE cliente set saldo = saldo + " . $cantidad . " where no_cuenta = " . $c2 . "; ");
   }
   return $var;
 }
