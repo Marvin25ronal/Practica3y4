0f2f04fbc35fbc7e8003221acdee7e99ff34377f
Marvin Martinez <marvin1ronal@gmail.com>
2019-10-27 18:27:16 -0600

    Prueba fallida jenkins

--- app-banco/general/Pruebas/Pruebas Informacion/InformacionTest.php
+++ app-banco/general/Pruebas/Pruebas Informacion/InformacionTest.php
@@ -1,12 +1,13 @@
 <?php
 use PHPUnit\Framework\TestCase;
 //prueba jenkins1
+//prueba fallida jenkins
 class pruebaInformacion extends TestCase
 {
   public function testInformacion(){
     include_once __DIR__."\../../BaseDatos/Consultas.php";
     $this->assertEquals(is_array(obtenerDatos(1)),true);
-    $this->assertEquals(is_array(obtenerDatos(100)),false);
+    $this->assertEquals(is_array(obtenerDatos(100)),true);
   }
 }
  ?>
