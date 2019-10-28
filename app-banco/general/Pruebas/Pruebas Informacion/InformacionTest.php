39723c0d734eada2a87ca99b58938ac02e9241d9
Marvin Martinez <marvin1ronal@gmail.com>
2019-10-27 18:29:08 -0600

    Corregir prueba jenkins

--- app-banco/general/Pruebas/Pruebas Informacion/InformacionTest.php
+++ app-banco/general/Pruebas/Pruebas Informacion/InformacionTest.php
@@ -2,12 +2,13 @@
 use PHPUnit\Framework\TestCase;
 //prueba jenkins1
 //prueba fallida jenkins
+//corregir prueba
 class pruebaInformacion extends TestCase
 {
   public function testInformacion(){
     include_once __DIR__."\../../BaseDatos/Consultas.php";
     $this->assertEquals(is_array(obtenerDatos(1)),true);
-    $this->assertEquals(is_array(obtenerDatos(100)),true);
+    $this->assertEquals(is_array(obtenerDatos(100)),false);
   }
 }
  ?>
