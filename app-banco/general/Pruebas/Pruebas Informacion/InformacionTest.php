449cdcada5d8727a5ccb70dfc8c170aad03b2a18
Marvin Martinez <marvin1ronal@gmail.com>
2019-10-27 18:13:38 -0600

    Cambio de nombres

--- app-banco/general/Pruebas/Pruebas Informacion/InformacionTest.php
+++ app-banco/general/Pruebas/Pruebas Informacion/InformacionTest.php
@@ -1,7 +1,7 @@
 <?php
 use PHPUnit\Framework\TestCase;
 
-class pruebaCambio extends TestCase
+class pruebaInformacion extends TestCase
 {
   public function testInformacion(){
     include_once __DIR__."\../../BaseDatos/Consultas.php";
